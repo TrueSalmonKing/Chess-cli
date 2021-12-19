af940da9cc78d4c9b4855d1a52a062b9be503f00
TrueSalmonKing <hichameddoubi@gmail.com>
2021-12-19 17:16:47 +0100

    Knight movement implemented

--- board.c
+++ board.c
@@ -4,12 +4,12 @@
 #include <regex.h>
 
 void next_move(char board[8][8][4], char* move, size_t buff_size);
-void current_board(const char board[8][8][4], const char* next_move);
-int check_move(char* move);
+void current_board(char board[8][8][4], const char* next_move);
+int check_move(char board[8][8][4], char* curr_place, char* move);
 int main(int argc, char* argv[argc+1]) {
 
 //4 bytes to include both the unicode character (3bytes) and the null pointer (1byte), otherwise it will keep on printing characters from the rest of the adjacent strings
-	char board[8][8][4] = {
+	char board[8][8][4]={
 			{"\u265c","\u265e","\u265d","\u265b","\u265a","\u265d","\u265e","\u265c"}
 			,{"\u265f","\u265f","\u265f","\u265f","\u265f","\u265f","\u265f","\u265f"}
 			,{" "," "," "," "," "," "," "," "}
@@ -20,9 +20,8 @@ int main(int argc, char* argv[argc+1]) {
 			,{"\u2656","\u2658","\u2657","\u2655","\u2654","\u2658","\u2657","\u2656"}
 			};
 
-
-	size_t buff_size = 7;
-	char * current_move = (char *)malloc(sizeof(char)*buff_size);
+	size_t buff_size=7;
+	char * current_move=(char *)malloc(sizeof(char)*buff_size);
 
 	while(1){
 		current_board(board, current_move);
@@ -33,7 +32,7 @@ int main(int argc, char* argv[argc+1]) {
 
 }
 
-void current_board(const char board[8][8][4], const char* next_move){
+void current_board(char board[8][8][4], const char* next_move){
 	printf(" _a_b_c_d_e_f_g_h\n");
 	for(size_t i=0;i<8;i++){
 		printf("%lu|",i+1);
@@ -62,22 +61,38 @@ void next_move(char board[8][8][4], char* move, size_t buff_size){
 	move_2=strtok(NULL,delim);
 
 	printf("%s%s\n",move_1,move_2);
-	if(!check_move(move_1) && !check_move(move_2))
+	if(check_move(board, move_1, move_2)) {
 		printf("valid move !\n");
-//chess piece movement	
+		
+	}
 }
 
-int check_move(char* curr_place, char* move){
-	
+int check_move(char board[8][8][4], char* curr_place, char* move){
+	int cpx=curr_place[2]-'1';
+	int cpy=curr_place[1]-'a';
+	int mx=move[2]-'1';
+	int my=move[1]-'a';
+
+	int a=my-cpy;
+	int b=mx-cpx;
 	int rc;
 	regex_t regex;
 
 	rc=regcomp(&regex, "[RNBKQP][a-h][1-8]",0);
+
+// !!!!!!!!!!!!!!! MUST CHECK WHY REGEX DOESN'T ACCEPT ?
 //	rc=regcomp(&regex, "[RNBKQ]?[a-h][1-8]",0);
-	rc = regexec(&regex,move,0,NULL,0);
+	rc=regexec(&regex,move,0,NULL,0);
 
 	switch (move[0]) {
 		case 'N':
-			if( ((move[1]-'a')<8)&&(move[2]<8) && !( ((  ) )
+			if(!strcmp(board[cpx][cpy],"\u265e") && !(((my)>>3)&&((mx)>>3)) && (((b&2)&&(a&1))^((b&1)&&(a&2)))) {
+//				printf("mx=%d my=%d cpx=%d cpy%d",mx,my,cpx,cpy);
+				strcpy(board[mx][my],board[cpx][cpy]);
+				strcpy(board[cpx][cpy]," ");
+				return 0;
+			};
+		default:
+			return rc;
 	}
 }
